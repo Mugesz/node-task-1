c25849bb8dbc0d17ceba2820a2e8bac7803b6fc8
frekerzzzz <kannamugesh135@gmail.com>
2023-12-13 14:54:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("timestamps"));
+
+let dateString = new Date().toUTCString().slice(0, -3);
+
+const folderName = '/users/kanna/node-task-1/timestamp';
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+} catch (err) {
+  console.error(err);
+}
+
+ fs.writeFileSync("timestamp/date-time.txt", dateString )
+
+ const fileName = path.join('/timestamp','date-time.txt')
+
+app.get("/", function (req, res) {
+  res.send(`The date and time shold be in ${fileName} : folder go to localhost 3007/static to view the updated time`);
+});
+
+app.get("/static", (req, res) => {
+
+  res.send(`Last created timestamp ${dateString}`);
+});
+
+app.listen(3007);
